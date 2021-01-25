d4b803e5b4ac98a77bc89c4130edf5de8120e1f5
MarioDuval <77450981+MarioDuval@users.noreply.github.com>
2021-01-25 18:30:00 +0100

    Activitats clase

---
+++ functions.py
@@ -0,0 +1,11 @@
+def file_write(fname):
+    f = open(fname, "a+")
+    str = input("Introdueix un text: ")
+    f.write(str)
+    f.close()
+
+def file_write(fname):
+    f = open(fname, "a+")
+    str = input("Introdueix un text: ")
+    f.write(str)
+    f.close()
---
+++ main.py
@@ -0,0 +1,9 @@
+import functions as f
+
+
+def main():
+    f.file_write('C:/Users/mario/PycharmProjects/Fitxers/recursivitat/text.txt')
+
+if __name__ == '__main__':
+    main()
+
