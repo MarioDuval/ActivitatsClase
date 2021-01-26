f985f05fe650e0280f41bb46ba7d1461570b823a
MarioDuval <77450981+MarioDuval@users.noreply.github.com>
2021-01-26 12:35:40 +0100

    Activitat clase

--- functions.py
+++ functions.py
@@ -4,8 +4,28 @@ def file_write(fname):
     f.write(str)
     f.close()
 
-def file_write(fname):
+def file_write1(fname):
     f = open(fname, "a+")
     str = input("Introdueix un text: ")
     f.write(str)
+    f.close()
+
+def file_name(fname):
+    fileName = input("Introdueix el nom de un fitxer: ")
+    try:
+        with open(fileName) as fname:
+            print(fname.readline())
+        fname.close()
+    except:
+        print("Error")
+
+def file_read(fname):
+    f = open(fname, "r")
+    print(f.read())
+    f.close()
+
+def file_write2(fname):
+    f = open('C:/Users/mario/PycharmProjects/Fitxers/recursivitat/text.txt', "w")
+    for i in range(10):
+        f.write("This is line %d\r\n" % (i + 1))
     f.close()
--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import functions as f
 
 
 def main():
-    f.file_write('C:/Users/mario/PycharmProjects/Fitxers/recursivitat/text.txt')
+    #f.file_write('C:/Users/mario/PycharmProjects/Fitxers/recursivitat/text.txt')
+    #f.file_write1('C:/Users/mario/PycharmProjects/Fitxers/recursivitat/text.txt')
+    #f.file_name('C:/Users/mario/PycharmProjects/Fitxers/recursivitat/text.txt')
+    #f.file_read('C:/Users/mario/PycharmProjects/Fitxers/recursivitat/text.txt')
+    f.file_write2('C:/Users/mario/PycharmProjects/Fitxers/recursivitat/text.txt')
 
 if __name__ == '__main__':
     main()
